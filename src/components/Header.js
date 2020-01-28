09eadb504fca407b78c5cc0a36192cb5ce131f23
Lilkgb <mcleanrs@comcast.net>
2020-01-27 20:51:20 -0800

    add logout button to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import * as firebase from 'firebase';
 
 function Header(){
+
+  function logOutUser(){
+    firebase.auth().signOut();
+  }
 
   return(
     <div className="header">
       <Link to='/'>Home</Link>
       <h2>Welcome New User</h2>
+      <button onClick={logOutUser}>Log Out</button>
     </div>
   )
 }

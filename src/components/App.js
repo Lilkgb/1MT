d07cbb31938a05a98151004aba39cd620e5986a8
Lilkgb <mcleanrs@comcast.net>
2020-01-27 19:24:33 -0800

    adjust loading gif to fit properly on all screens

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import '../scss/App.scss';
 import Error404 from './Error404';
 import Home from './Home';
@@ -7,23 +7,10 @@ import {connect} from 'react-redux';
 
 function App(props) {
 
-  const [loader, setLoader] = useState('maxWidth')
-
-  useEffect(() => {
-    let w = window.innerWidth;
-    let h = window.innerHeight;
-    if(w >= h){
-      setLoader("maxHeight")
-    } else {
-      setLoader("maxWidth")
-    }
-    console.log(loader)
-  })
-
   return (
     <div className="App">
       <div className="loadingGif">
-        <img className={loader} src={require('../assets/loading.gif')}/>
+        <img src={require('../assets/loading.gif')}/>
       </div>
       <Switch>
         <Route exact path ='/' component={Home}/>
